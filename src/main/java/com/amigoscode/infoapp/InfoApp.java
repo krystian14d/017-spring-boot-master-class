0925416138bd94fca8fcf2cbed7a9e79ac36c52f
Krystian <krystian14d@gmail.com>
2022-05-28 18:57:37 +0200

    @Value and @ConfigurationProperties annotations

---
+++ src/main/java/com/amigoscode/infoapp/InfoApp.java
@@ -0,0 +1,19 @@
+package com.amigoscode.infoapp;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@ToString
+@Configuration
+@ConfigurationProperties(prefix = "info.app")
+public class InfoApp {
+
+    private String name;
+    private String description;
+    private String version;
+}
